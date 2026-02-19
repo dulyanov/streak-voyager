a2260a57150ce5dac7351703c0765f050235ed83
Danila Ulyanov <danila@ulyanov.net>
2026-02-18 21:15:34 -0600

    Initial Commit

---
+++ StreakVoyage/StreakVoyageApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreakVoyageApp.swift
+//  StreakVoyage
+//
+//  Created by Danila Ulyanov on 2/18/26.
+//
+
+import SwiftUI
+
+@main
+struct StreakVoyageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StreakVoyageTests/StreakVoyageTests.swift
@@ -0,0 +1,17 @@
+//
+//  StreakVoyageTests.swift
+//  StreakVoyageTests
+//
+//  Created by Danila Ulyanov on 2/18/26.
+//
+
+import Testing
+@testable import StreakVoyage
+
+struct StreakVoyageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
